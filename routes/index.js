4bc1f93095621a9e32649314fa3c5eddbbffe516
Victor Vikhal <alfonso.vikhal@gmail.com>
2023-02-11 21:49:50 +0100

    En el bonus del lab

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const MovieModel = require("../models/Movie.model.js")
+
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => { 
+    
+    res.render('index.hbs')
+
+});
+
+// GET "/movies" page.
+
+router.get("/movies",(req, res, next) =>{
+
+    MovieModel.find()
+        .then((response) => {
+            console.log(response)
+            res.render("movies/movies.hbs", {
+                moviesList: response
+            })
+        })
+    
+        .catch((err) =>{
+            next(err)
+        })
+})
+
+// GET "/movie/:id" page
+
+router.get("/movie/:id", (req, res, next) => {
+    
+    const { id } = req.params
+
+    MovieModel.findById(id)
+    .then((response) => {
+        console.log(response)
+        res.render("movies/movieDetails.hbs", {
+            oneMovie: response
+        })
+    })
+    
+
+    .catch((err) => {
+        next(err)
+    })
+})
+
 
 module.exports = router;
